ea8ed814c9c811b21a6735a8a8edcd8819df2189
Holly Moody <hollymoody@charter.net>
2014-10-11 15:41:43 -0400

    Add category model, specs and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141011163835) do
+ActiveRecord::Schema.define(version: 20141011193608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20141011163835) do
   create_table "authors", force: true do |t|
     t.string   "name"
     t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Author.destroy_all
+Category.destroy_all
 a1 = Author.create(name: "Holly Moody", title: "Rockn' Rails Developer")
 a2 = Author.create(name: "Ronn Moody", title: "Supreme Commander")
 a3 = Author.create(name: "Levi Howard", title: "Sound Master")
+
+c1 = Category.create(name: "Funny")
+c2 = Category.create(name: "Useful")
+c3 = Category.create(name: "Unusual")
