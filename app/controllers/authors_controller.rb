a798259bec3dedf1dad07fbe9acbf98015ff3dcd
Holly Moody <hollymoody@charter.net>
2014-10-11 15:30:30 -0400

    Fix bug in author edit

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,14 +1,25 @@
 class AuthorsController < ApplicationController
-  before_action :set_author, only: [:show, :destroy]
+  before_action :set_author, only: [:show, :destroy, :edit, :update]
 
   def index
     @authors = Author.all
     @author = Author.new
   end
 
+  def edit
+  end
+
   def create
     @author = Author.create(author_params)
     redirect_to authors_path
+  end
+
+  def update
+    if @author.update(author_params)
+      redirect_to authors_path, notice: 'author was successfully saved'
+    else
+      redirect_to :edit
+    end
   end
 
   def destroy
